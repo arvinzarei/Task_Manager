8745e0646ad4181ad8ddd44802ef66195db08fd0
arvin zarei <arvinzarei123@gmail.com>
2021-05-09 16:19:24 +0430

    Complete Task Manager

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,33 +7,19 @@ use App\Models\Task;
 use App\Http\Requests\CreateTaskRequest;
 class TaskController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $tasks=Task::orderBy('id','DESC')->get();
         return view('task.index',compact('tasks'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         return view('task.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(CreateTaskRequest $request)
     {
         Task::create([
@@ -43,47 +29,19 @@ class TaskController extends Controller
     }
     
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Task $task)
     {
         return view('task.edit',compact('task'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request,Task $task)
     {
         $task->update($request->all());
         return redirect()->route('task.index')->with('success','! ویرایش انجام شد');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(Task $task)
     {
         $task->delete();
